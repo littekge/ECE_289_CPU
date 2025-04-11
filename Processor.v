879f4ba63f58147dd41c9adcc42a7328a489598f
littekge <littekge@miamioh.edu>
2025-04-11 09:40:37 -0400

    feat: started work on processor file

--- Processor.v
+++ Processor.v
@@ -87,8 +87,70 @@ module Processor (
 	//////////// GPIO_1, GPIO_1 connect to GPIO Default //////////
 	// inout 		    [35:0]		GPIO_1
 );
+//clk and rst
+reg clk;
+assign clk = CLOCK_50;
+reg rst;
+assign rst = KEY[0];
 
+//registers
+reg [31:0]x0;
+reg [31:0]x1;
+reg [31:0]x2;
+reg [31:0]x3;
+reg [31:0]x4;
+reg [31:0]x5;
+reg [31:0]x6;
+reg [31:0]x7;
+reg [31:0]x8;
+reg [31:0]x9;
+reg [31:0]x10;
+reg [31:0]x11;
+reg [31:0]x12;
+reg [31:0]x13;
+reg [31:0]x14;
+reg [31:0]x15;
+reg [31:0]x16;
+reg [31:0]x17;
+reg [31:0]x18;
+reg [31:0]x19;
+reg [31:0]x20;
+reg [31:0]x21;
+reg [31:0]x22;
+reg [31:0]x23;
+reg [31:0]x24;
+reg [31:0]x25;
+reg [31:0]x26;
+reg [31:0]x27;
+reg [31:0]x28;
+reg [31:0]x29;
+reg [31:0]x30;
+reg [31:0]x31;
+reg [31:0]pc;
 
+//hardwiring zero register
+assign x0 = 32'd0;
+
+//fsm state variables
+parameter FSM_SIZE = 'd8;
+reg [FSM_SIZE-1:0]S, NS;
+
+//possible fsm states
+parameter START = 8'd0;
+
+always @ (posedge clk or negedge rst) begin
+
+end
+
+always @ (*) begin
+
+end
+	
+always @ (posedge clk or negedge rst) begin
+
+end
+	
+	
 
 
 endmodule
