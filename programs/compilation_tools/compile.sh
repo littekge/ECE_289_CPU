6f3b735a7116e0a9786cb49537fed1ee17e183bd
littekge <littekge@miamioh.edu>
2025-05-13 23:22:53 -0400

    feat: updating c pipeline

--- programs/compilation_tools/compile.sh
+++ programs/compilation_tools/compile.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 cd ../
-/opt/riscv/bin/riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -nostdlib -ffreestanding -T compilation_tools/memory_map.ld -o $1.rv32.elf $1.c
-/opt/riscv/bin/riscv64-unknown-elf-objdump -D $1.rv32.elf
+/opt/riscv/bin/riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -ffunction-sections -nostdlib -ffreestanding -T compilation_tools/memory_map.ld -o $1.rv32.elf $1.c
+/opt/riscv/bin/riscv64-unknown-elf-objdump -D $1.rv32.elf > assembly.txt
 /usr/local/bin/riscv64-unknown-elf-elf2hex --bit-width 32 --input $1.rv32.elf --output ../memory/program.hex
 rm $1.rv32.elf
