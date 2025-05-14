19f87f0eb229488a81c64b2b8889652fbf166e35
littekge <littekge@miamioh.edu>
2025-05-13 20:33:49 -0400

    fix: program converter

--- programs/compilation_tools/compile.sh
+++ programs/compilation_tools/compile.sh
@@ -2,5 +2,5 @@
 cd ../
 /opt/riscv/bin/riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -nostdlib -ffreestanding -T compilation_tools/memory_map.ld -o $1.rv32.elf $1.c
 /opt/riscv/bin/riscv64-unknown-elf-objdump -D $1.rv32.elf
-/opt/riscv/bin/riscv64-unknown-elf-objcopy -O ihex $1.rv32.elf ../memory/program.hex
+/usr/local/bin/riscv64-unknown-elf-elf2hex --bit-width 32 --input $1.rv32.elf --output ../memory/program.hex
 rm $1.rv32.elf
