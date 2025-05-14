58d142f6db37d8e13a89c5d3eb5d574be13c9617
BuildTools <unconfigured@null.spigotmc.org>
2025-05-13 21:47:56 -0400

    feat: finalized c compilation pipeline

--- programs/compilation_tools/compile.sh
+++ programs/compilation_tools/compile.sh
@@ -2,5 +2,5 @@
 cd ../
 /opt/riscv/bin/riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -nostdlib -ffreestanding -T compilation_tools/memory_map.ld -o $1.rv32.elf $1.c
 /opt/riscv/bin/riscv64-unknown-elf-objdump -D $1.rv32.elf
-/usr/local/bin/riscv64-unknown-elf-elf2hex --bit-width 32 --input $1.rv32.elf --output ../memory/program.hex
+/usr/local/bin/riscv64-unknown-elf-elf2hex --bit-width 32 --input $1.rv32.elf --output compilation_tools/program.hex
 rm $1.rv32.elf
